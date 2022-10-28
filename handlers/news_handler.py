30acc80c271489b44909eba9a3b6c9b13b536210
Mr Dao <daogiatuank54@gmail.com>
2022-10-28 12:50:43 +0700

    add features

---
+++ handlers/news_handler.py
@@ -0,0 +1,50 @@
+from webbrowser import open
+
+import feedparser
+from bs4 import BeautifulSoup
+from rich import print
+
+
+class FeedHandler:
+    def __init__(self, feed_url: str = "https://vnexpress.net/rss/kinh-doanh.rss", limit_page: int = 10):
+        print('Use this fee_url:', feed_url, 'limit page:', limit_page, 'as default')
+        self.payload = feedparser.parse(feed_url)
+        self.title = self.payload.feed.title
+        self.entries = self.payload.entries
+        self.feed_url = feed_url
+        self.limit_page = limit_page
+
+    def get_news(self):
+        feed_url: str = self.feed_url
+        limit_page: int = self.limit_page
+        entries: list = self.entries
+
+        index = 0
+        for entry in entries:
+            if index < limit_page:
+                # prepare
+                order = '[bold red]' + str(index + 1) + '[/bold red]'
+                title = f'[link={entry.link}][green]' + entry.title + '[green][/link]'
+                content = '[white]' + BeautifulSoup(entry.summary, features="html.parser").text + '[/white]'
+                date = entry.published_parsed
+                posted_date = ('Posted at:', str(date.tm_year) + '-' + str(date.tm_mon) + '-' + str(date.tm_mday), str(
+                    date.tm_hour).zfill(2) + ':' + str(date.tm_min).zfill(2))
+                # print
+                print('----***-----')
+                print(*posted_date)
+                print(order, title)
+                print(content)
+
+                index += 1
+            else:
+                break
+
+        self.open_page(entries)
+
+    def open_page(self, entries: list):
+        try:
+            get_page = int(input('Do you want to open anything, type number of page \n'))
+            open(entries[int(get_page) - 1].link)
+        except ValueError:
+            print("Input is not of int type , please retry")
+            return self.open_page(entries)
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from webbrowser import open
-
-import feedparser
 import typer
-from bs4 import BeautifulSoup
 from rich import print
 
+from handlers.database_handler import SqliteHandler
+from handlers.news_handler import FeedHandler
+
 app = typer.Typer()
-
-
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+db_handler = SqliteHandler("local_app.db")
 
 
 @app.command()
@@ -19,40 +14,23 @@ def goodbye(name: str):
 
 
 @app.command()
+def input_rss(rss_link: str):
+    feed_title = FeedHandler(feed_url=rss_link).title
+    db_handler.insert_feed(feed_title, rss_link)
+
+
+@app.command()
+def rss():
+    db_handler.list_feeds()
+
+
+@app.command()
 def get_news(limit: int = typer.Argument(10)):
-    # Use a breakpoint in the code line below to debug your script.
-    NewsFeed = feedparser.parse("https://vnexpress.net/rss/kinh-doanh.rss")
-    entries = NewsFeed.entries
-    index = 0
-    for entry in entries:
-        if index < limit:
-            # prepare
-            order = '[bold red]' + str(index + 1) + '[/bold red]'
-            title = f'[link={entry.link}][green]' + entry.title + '[green][/link]'
-            content = '[white]' + BeautifulSoup(entry.summary, features="html.parser").text + '[/white]'
-            date = entry.published_parsed
-            posted_date = ('Posted at:', str(date.tm_year) + '-' + str(date.tm_mon) + '-' + str(date.tm_mday), str(
-                date.tm_hour).zfill(2) + ':' + str(date.tm_min).zfill(2))
-            # print
-            print('----***-----')
-            print(*posted_date)
-            print(order, title)
-            print(content)
-
-            index += 1
-        else:
-            break
-
-    open_page(entries)
-
-
-def open_page(entries: list):
-    try:
-        get_page = int(input('Do you want to open anything, type number of page \n'))
-        open(entries[int(get_page) - 1].link)
-    except ValueError:
-        print("Input is not of int type , please retry")
-        return open_page(entries)
+    feeds: list = db_handler.get_feeds()
+    db_handler.list_feeds()
+    feed_index = int(input("Type your order of feed to get \n"))
+    feed_handler: FeedHandler = FeedHandler(feeds[feed_index-1][1],limit_page=limit)
+    feed_handler.get_news()
 
 
 if __name__ == '__main__':
