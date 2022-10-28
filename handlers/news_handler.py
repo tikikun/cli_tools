28c3067820269d65aa208394410244dc44c1ea9e
Mr Dao <daogiatuank54@gmail.com>
2022-10-28 15:42:13 +0700

    add feature

--- handlers/news_handler.py
+++ handlers/news_handler.py
@@ -3,7 +3,7 @@ from webbrowser import open
 import feedparser
 from bs4 import BeautifulSoup
 from rich import print
-
+from rich.panel import Panel
 
 class FeedHandler:
     def __init__(self, feed_url: str = "https://vnexpress.net/rss/kinh-doanh.rss", limit_page: int = 10):
@@ -24,7 +24,7 @@ class FeedHandler:
                 # prepare
                 order = '[bold red]' + str(index + 1) + '[/bold red]'
                 title = f'[link={entry.link}][green]' + entry.title + '[green][/link]'
-                content = '[white]' + BeautifulSoup(entry.summary, features="html.parser").text + '[/white]'
+                content = BeautifulSoup(entry.summary, features="html.parser").text
                 date = entry.published_parsed
                 posted_date = ('Posted at:', str(date.tm_year) + '-' + str(date.tm_mon) + '-' + str(date.tm_mday), str(
                     date.tm_hour).zfill(2) + ':' + str(date.tm_min).zfill(2))
@@ -32,7 +32,7 @@ class FeedHandler:
                 print('----***-----')
                 print(*posted_date)
                 print(order, title)
-                print(content)
+                print(Panel(content))
 
                 index += 1
             else:
